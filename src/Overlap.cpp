56aa8cda126ab8bca5b948180fa6acb08ff83152
Ye-Eun Myung <MYE.flute1102@gmail.com>
2016-02-26 14:14:41 -0800

    blocks move down

--- src/Overlap.cpp
+++ src/Overlap.cpp
@@ -7,22 +7,24 @@ bool overlap(sf::Sprite& sprite1, sf::Sprite& sprite2)
 
 	// Rectangles with negative dimensions are allowed, so we must handle them correctly
 	// Compute the min and max of the first rectangle on both axes
-	float r1MinX = std::min(rectangle1.left, rectangle1.left + rectangle1.width);
-	float r1MaxX = std::max(rectangle1.left, rectangle1.left + rectangle1.width);
-	float r1MinY = std::min(rectangle1.top, rectangle1.top + rectangle1.height);
-	float r1MaxY = std::max(rectangle1.top, rectangle1.top + rectangle1.height);
+	float r1MinX = std::min(rectangle1.left, rectangle1.left + rectangle1.width); //left
+	float r1MaxX = std::max(rectangle1.left, rectangle1.left + rectangle1.width);//right
+	float r1MinY = std::min(rectangle1.top, rectangle1.top + rectangle1.height); // top 
+	float r1MaxY = std::max(rectangle1.top, rectangle1.top + rectangle1.height); // bottom of the maincharactersprite
 
 	// Compute the min and max of the second rectangle on both axes
-	float r2MinX = std::min(rectangle2.left, rectangle2.left + rectangle2.width);
-	float r2MaxX = std::max(rectangle2.left, rectangle2.left + rectangle2.width);
-	float r2MinY = std::min(rectangle2.top, rectangle2.top + rectangle2.height);
-	float r2MaxY = std::max(rectangle2.top, rectangle2.top + rectangle2.height);
+	float r2MinX = std::min(rectangle2.left, rectangle2.left + rectangle2.width);//left
+	float r2MaxX = std::max(rectangle2.left, rectangle2.left + rectangle2.width);//right
+	float r2MinY = std::min(rectangle2.top, rectangle2.top + rectangle2.height);//top of the blocksprite
+	float r2MaxY = std::max(rectangle2.top, rectangle2.top + rectangle2.height); // bottom
 
 	// Compute the intersection boundaries
 	float interLeft = std::max(r1MinX, r2MinX);
 	float interTop = std::max(r1MinY, r2MinY);
 	float interRight = std::min(r1MaxX, r2MaxX);
 	float interBottom = std::min(r1MaxY, r2MaxY);
+
+	//want to check if bottom and top are overlaping
 
 	// If the intersection is valid (positive non zero area), then there is an intersection
 	return ((interLeft <= interRight) && (interTop <= interBottom));
--- src/main.cpp
+++ src/main.cpp
@@ -9,13 +9,13 @@
 using namespace std;
 
 void OpeningSceneMode(sf::RenderWindow&);
-void PlayMode(sf::RenderWindow&, sf::Sprite&, vector<sf::Sprite>, sf::Texture);
+void PlayMode(sf::RenderWindow&, sf::Sprite&, vector<sf::Sprite>, vector<sf::Sprite>);
 
 int main()
 {
 	// initalizing window
 	sf::RenderWindow window(sf::VideoMode(WidthWindow, LengthWindow), "Jumping Anthony");
-	window.setFramerateLimit(60);
+	window.setFramerateLimit(20);
 	
 	// initalizing a texture object
 	sf::Texture mainCharacterTexture;
@@ -27,16 +27,29 @@ int main()
 	mainCharacterSprite.setScale(0.1, 0.1);
 	mainCharacterSprite.setTexture(mainCharacterTexture);
 	mainCharacterSprite.setPosition(100, 600);
-	
+
 	sf::Texture blockTexture;
 	blockTexture.loadFromFile(resourcePath() + "assets\\block.png");
 	vector<sf::Sprite> platform;
 
-	for (int i = 0; i < 4; i++)
+	for (int i = 0; i < 4; i++) //platform
 	{
 		platform.push_back(sf::Sprite(blockTexture));
 		platform[i].setPosition(sf::Vector2f(i * 120, 700));
+		//platform[i].setScale(sf::Vector2f(0.1, 0.1));
 	}
+	int numBlock = 0;
+	vector<sf::Sprite> block;
+
+	while (numBlock < 100)
+	{
+		block.push_back(sf::Sprite(blockTexture));
+		block[numBlock].setOrigin(sf::Vector2f(60, 20));
+		block[numBlock].setPosition(sf::Vector2f((rand() % 280 + 1), 600 - (150 * numBlock)));
+		//block[numBlock].setScale(sf::Vector2f(0.1, 0.1));
+		numBlock++;
+	}
+
 	gameState = OPENING;
 
 	while (window.isOpen())
@@ -47,7 +60,7 @@ int main()
 			OpeningSceneMode(window);
 			break;
 		case PLAY:
-			PlayMode(window, mainCharacterSprite, platform, blockTexture);
+			PlayMode(window, mainCharacterSprite, platform, block);
 			break;
 		}
 		
@@ -75,16 +88,14 @@ void OpeningSceneMode(sf::RenderWindow &window)
 }
 
 //PLAY MODE EVENT HANDLE
-void PlayMode(sf::RenderWindow &window, sf::Sprite &mainCharacterSprite, vector<sf::Sprite> platform, sf::Texture blockTexture)
+void PlayMode(sf::RenderWindow &window, sf::Sprite &mainCharacterSprite, vector<sf::Sprite> platform, vector<sf::Sprite> block)
 {
 	sf::Event event;
 	const float GRAVITY = 1;
 	sf::Vector2f velocity(sf::Vector2f(0, 0));
-	float moveSpeed = 5.0f, jumpSpeed = 20.0f;
+	float moveSpeed = 5.0f, jumpSpeed = 15.0f;
 	srand(time(NULL));
 
-	int numBlock = 0;
-	vector<sf::Sprite> block;
 	while (window.isOpen())
 	{
 		while (window.pollEvent(event))
@@ -92,17 +103,14 @@ void PlayMode(sf::RenderWindow &window, sf::Sprite &mainCharacterSprite, vector<
 			if (event.type == sf::Event::Closed)
 				window.close();
 		}
-		if (numBlock == 0)
+		for (int k = 0; k < 4; k++)
 		{
-			while (numBlock < 4)
-			{
-				block.push_back(sf::Sprite(blockTexture));
-				block[numBlock].setOrigin(sf::Vector2f(60, 20));
-				block[numBlock].setPosition(sf::Vector2f((rand() % 280 + 1), 600 - (200 * numBlock)));
-				numBlock++;
-			}
+			platform[k].move(sf::Vector2f(0, 20));
 		}
-
+		for (int k = 0; k < 100; k++)
+		{
+			block[k].move(sf::Vector2f(0, 20));
+		}
 		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) //stops when the key is not pressed
 			velocity.x = -moveSpeed;
 		else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
@@ -110,42 +118,47 @@ void PlayMode(sf::RenderWindow &window, sf::Sprite &mainCharacterSprite, vector<
 		else
 			velocity.x = 0;
 
-		//sf::Vector2f currentPos = mainCharacterSprite.getPosition();
-		//if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
-		//	velocity.y = -jumpSpeed;
-
 		//when shape touches the block the shape should jump
 		for (int x = 0; x < 4; x++)
 		{
-			if (overlap(mainCharacterSprite, platform[x]))
-			{
-				velocity.y = -jumpSpeed;
-			}
-			mainCharacterSprite.move(velocity.x, velocity.y);
-			if (mainCharacterSprite.getPosition().y < platform[x].getPosition().y ||
-				mainCharacterSprite.getPosition().y < block[x].getPosition().y) // groundposition
+				if (overlap(mainCharacterSprite, platform[x]))
+				{
+					velocity.y = -jumpSpeed;
+				}
+				mainCharacterSprite.move(velocity.x, velocity.y);
+				if (mainCharacterSprite.getPosition().y < platform[x].getPosition().y) // groundposition
+				{
+					velocity.y += GRAVITY; // if the character is above the block, add the gravity to the y-value in velocity
+				}
+				else if (velocity.y > 0 && overlap(mainCharacterSprite, platform[x])) //if overlap with platform setposition
+				{
+					mainCharacterSprite.setPosition(mainCharacterSprite.getPosition().x, platform[x].getPosition().y);
+				}
+		}
+		for (int elem = 0; elem < 100; elem++)
+		{
+			if(mainCharacterSprite.getPosition().y < block[elem].getPosition().y) // groundposition
 			{
 				velocity.y += GRAVITY; // if the character is above the block, add the gravity to the y-value in velocity
 			}
-			else if (overlap(mainCharacterSprite, platform[x])) //if overlap with platform setposition
+			else if (velocity.y > 0 && overlap(mainCharacterSprite, block[elem])) //if overlap with one of the blocks setposition
 			{
-				mainCharacterSprite.setPosition(mainCharacterSprite.getPosition().x, platform[x].getPosition().y);
+				mainCharacterSprite.setPosition(mainCharacterSprite.getPosition().x, block[elem].getPosition().y);
 			}
-			else if (overlap(mainCharacterSprite, block[x])) //if overlap with one of the blocks setposition
-			{
-				mainCharacterSprite.setPosition(mainCharacterSprite.getPosition().x, block[x].getPosition().y);
-			}
-			if (velocity.y > 0 && overlap(mainCharacterSprite, block[x])) //when the velocity is greater than 0 and character overlap with one of the block, jump
+			if (velocity.y > 0 && overlap(mainCharacterSprite, block[elem])) //when the velocity is greater than 0 and character overlap with one of the block, jump
 			{
 				velocity.y = -jumpSpeed;
 			}
-
 		}
+
 		window.clear();
 		window.draw(mainCharacterSprite);
 		for (int k = 0; k < 4; k++)
 		{
 			window.draw(platform[k]);
+		}
+		for (int k = 0; k < 100; k++)
+		{
 			window.draw(block[k]);
 		}
 
